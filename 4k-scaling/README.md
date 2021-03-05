76730dffc7eaa442f9b0f961303e23c03f0c97f2
Mathias Lund Ahrn <golvkaldt@gmail.com>
2021-03-05 13:58:00 +0100

    run_scaled script

---
+++ 4k-scaling/README.md
@@ -0,0 +1,4 @@
+# App scaling
+
+1. Install app scaling
+2. Run the app `run_scaled <app_name>`
---
+++ 4k-scaling/app-scaling.sh
@@ -0,0 +1,5 @@
+sudo apt -y install xvfb xpra
+wget https://raw.githubusercontent.com/kaueraal/run_scaled/master/run_scaled
+sudo mv run_scaled /usr/local/bin
+chmod +x /usr/local/bin
+
